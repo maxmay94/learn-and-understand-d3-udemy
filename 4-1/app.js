1ef9a1cd8af142b66ac6423bc74cbce4a94c2915
maxmay94 <maxmay94@gmail.com>
2022-09-23 16:03:01 -0400

    draw blank svg on canvas

--- 4-1/app.js
+++ 4-1/app.js
@@ -1,5 +1,18 @@
 async function draw() {
+  // Data
   let dataset = await d3.json('data.json')
+
+  // Dimensions
+  const dimensions = {
+    width: 800,
+    height: 800
+  }
+
+  // Draw Image
+  const svg = d3.select('#chart')
+    .append('svg')
+    .attr('width', dimensions.width)
+    .attr('height', dimensions.height)
 }
 
 draw()
