a339735ddb0c0a9281b737661da7f95185c3a254
maxmay94 <maxmay94@gmail.com>
2022-09-23 16:10:52 -0400

    add margin

--- 4-1/app.js
+++ 4-1/app.js
@@ -5,7 +5,13 @@ async function draw() {
   // Dimensions
   const dimensions = {
     width: 800,
-    height: 800
+    height: 800,
+    margin: {
+      top: 50,
+      bottom: 50,
+      left: 50,
+      right: 50
+    }
   }
 
   // Draw Image
@@ -13,6 +19,17 @@ async function draw() {
     .append('svg')
     .attr('width', dimensions.width)
     .attr('height', dimensions.height)
+
+  // Add Margin To Container
+  const ctr = svg.append('g')
+    .attr(
+      'transform', 
+      `translate(${dimensions.margin.left}, ${dimensions.margin.top})`
+      )
+  
+  // Draw Circle
+  ctr.append('circle')
+    .attr('r', 15)
 }
 
 draw()
