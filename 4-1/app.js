ce70ca586dca16abca2248204a1bbec337e5a301
maxmay94 <maxmay94@gmail.com>
2022-09-23 17:13:23 -0400

    add yAxis

--- 4-1/app.js
+++ 4-1/app.js
@@ -36,39 +36,52 @@ async function draw() {
   
   //Scales
   const xScale = d3.scaleLinear()
-      .domain(d3.extent(dataset, xAccessor))
-      .rangeRound([0, dimensions.ctrWidth])
-      .clamp(true)
+    .domain(d3.extent(dataset, xAccessor))
+    .rangeRound([0, dimensions.ctrWidth])
+    .clamp(true)
 
   const yScale = d3.scaleLinear()
-      .domain(d3.extent(dataset, yAccessor))
-      .rangeRound([0, dimensions.ctrHeight])
-      .nice()
-      .clamp(true)
+    .domain(d3.extent(dataset, yAccessor))
+    .rangeRound([0, dimensions.ctrHeight])
+    .nice()
+    .clamp(true)
   
   // Draw Circles
   ctr.selectAll('circle')
-      .data(dataset)
-      .join('circle')
-      .attr('cx', d => xScale(xAccessor(d)))
-      .attr('cy', d => yScale(yAccessor(d)))
-      .attr('r', 5)
-      .attr('fill', 'red')
+    .data(dataset)
+    .join('circle')
+    .attr('cx', d => xScale(xAccessor(d)))
+    .attr('cy', d => yScale(yAccessor(d)))
+    .attr('r', 5)
+    .attr('fill', 'red')
 
   // Axes
   const xAxis = d3.axisBottom(xScale)
-  // const yAxis = d3.axisLeft(yScale)
-
+  
   const xAxisGroup = ctr.append('g')
-      .call(xAxis)
-      .style('transform', `translateY(${dimensions.ctrHeight}px)`)
-      .classed('axis', true)
+    .call(xAxis)
+    .style('transform', `translateY(${dimensions.ctrHeight}px)`)
+    .classed('axis', true)
   
   xAxisGroup.append('text')
-      .attr('x', dimensions.ctrWidth / 2)
-      .attr('y', dimensions.margin.bottom - 10)
-      .attr('fill', 'black')
-      .text('Humidity')
+    .attr('x', dimensions.ctrWidth / 2)
+    .attr('y', dimensions.margin.bottom - 10)
+    .attr('fill', 'black')
+    .text('Humidity')
+  
+  const yAxis = d3.axisLeft(yScale)
+
+  const yAxisGroup = ctr.append('g')
+    .call(yAxis)
+    .classed('axis', true)
+
+  yAxisGroup.append('text')
+    .attr('x', -dimensions.ctrHeight / 2)
+    .attr('y', -dimensions.margin.left + 15)
+    .attr('fill', 'black')
+    .html('Temperature &deg; F')
+    .style('transform', 'rotate(270deg)')
+    .style('text-anchor', 'middle')
 }
 
 draw()
