39ddf42cfeb84e6409783b6ade3fd1805f75ae21
maxmay94 <maxmay94@gmail.com>
2022-09-23 12:03:13 -0400

    formatting

--- 3-1/app.js
+++ 3-1/app.js
@@ -1,5 +1,8 @@
 // const pBrowser = document.querySelector('p')
-const el = d3.select('body').append('p').attr('class', 'foo').text('Hello World')
+const el = d3.select('body')
+  .append('p')
+  .attr('class', 'foo')
+  .text('Hello World')
 
 // console.log(pBrowser)
 console.log(el)
