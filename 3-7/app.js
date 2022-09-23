b745c35e5e501bbcca16b5de69dad4c8d68c27a8
maxmay94 <maxmay94@gmail.com>
2022-09-23 12:36:39 -0400

    enter, update, exit

--- 3-7/app.js
+++ 3-7/app.js
@@ -3,7 +3,14 @@ const data = [10,20,30,40,50]
 const el = d3.select('ul')
   .selectAll('li')
   .data(data)
-  .join('li')
+  .join(
+    enter => {
+      return enter.append('li')
+        .style('color', 'purple')
+    },
+    update => update.style('color', 'green'),
+    exit => exit.remove()
+  )
   .text(d => d)
 
 console.log(el)
