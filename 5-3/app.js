f28a27f377ae0503bb423e2a2dd86c92fe451142
maxmay94 <maxmay94@gmail.com>
2022-09-24 11:48:48 -0400

    setup heatmap

--- 5-3/app.js
+++ 5-3/app.js
@@ -6,13 +6,29 @@ async function draw(el) {
   let dimensions = {
     width: 600,
     height: 150,
-  };
+  }
+
+  const box = 30
 
   // Draw Image
   const svg = d3.select(el)
     .append("svg")
     .attr("width", dimensions.width)
     .attr("height", dimensions.height)
+
+  // Rectangles
+  svg.append('g')
+  .attr('transform', 'translate(2,2)')
+    .attr('stroke', 'black')
+    .attr('fill', '#ddd')
+    .selectAll('rect')
+    .data(dataset)
+    .join('rect')
+    .attr('width', box - 3)
+    .attr('height', box - 3)
+    .attr('x', (d, i) => box * (i % 20))
+    .attr('y', (d, i) => box * ((i / 20) | 0))
+
 }
 
 draw('#heatmap1')
