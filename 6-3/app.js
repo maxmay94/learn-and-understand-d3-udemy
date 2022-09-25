cd458c159fcfe2f8a427112faa29fdee9c56df3a
maxmay94 <maxmay94@gmail.com>
2022-09-25 12:08:23 -0400

    add labels and axis

--- 6-3/app.js
+++ 6-3/app.js
@@ -3,6 +3,7 @@ async function draw() {
   const dataset = await d3.json('data.json')
 
   const xAccessor = d => d.currently.humidity
+  const yAccessor = d => d.length
 
   // Dimensions
   let dimensions = {
@@ -32,13 +33,19 @@ async function draw() {
     .range([0, dimensions.ctrWidth])
     .nice()
   
+    
   const bin = d3.bin()
-    .domain(xScale.domain())
-    .value(xAccessor)
-    .thresholds(10)
+  .domain(xScale.domain())
+  .value(xAccessor)
+  .thresholds(10)
   
   const newDataset = bin(dataset)
   const padding = 1
+  
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(newDataset, yAccessor)])
+    .range([dimensions.ctrHeight, 0])
+    .nice()
 
   // console.log({original: dataset, new: newDataset})
   
@@ -47,9 +54,27 @@ async function draw() {
     .data(newDataset)
     .join('rect')
     .attr('width', d => d3.max([0, xScale(d.x1) - xScale(d.x0)]) - padding)
-    .attr('height', 100)
+    .attr('height', d => dimensions.ctrHeight - yScale(yAccessor(d)))
     .attr('x', d => xScale(d.x0))
-    .attr('y', 0)
+    .attr('y', d => yScale(yAccessor(d)))
+    .attr('fill', '#01c5c4')
+
+  ctr.append('g')
+    .classed('bar-labels', true)
+    .selectAll('text')
+    .data(newDataset)
+    .join('text')
+    .attr('x', d => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
+    .attr('y', d => yScale(yAccessor(d)) - 10)
+    .text(yAccessor)
+
+  // Draw Axis
+  const xAxis = d3.axisBottom(xScale)
+
+  const xAxisGroup = ctr.append('g')
+    .style('transform', `translateY(${dimensions.ctrHeight}px)`)
+  
+  xAxisGroup.call(xAxis)
 }
 
 draw()
