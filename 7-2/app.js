e5970f144db8334a28404724ada7d901e2dfa2ba
maxmay94 <maxmay94@gmail.com>
2022-09-26 10:36:16 -0400

    add axis

--- 7-2/app.js
+++ 7-2/app.js
@@ -38,7 +38,35 @@ async function draw() {
     .domain(d3.extent(dataset, xAccessor))
     .range([0, dimensions.ctrWidth])
 
-  console.log(xScale(xAccessor(dataset[0])), dataset[0])
+  // console.log(xScale(xAccessor(dataset[0])), dataset[0])
+
+  // Generator
+  const lineGenerator = d3.line()
+    .x((d) => xScale(xAccessor(d)))
+    .y((d) => yScale(yAccessor(d)))
+
+  // console.log(lineGenerator(dataset))
+
+  ctr.append('path')
+    .datum(dataset)
+    .attr('d', lineGenerator)
+    .attr('fill', 'none')
+    .attr('stroke', '#30475e')
+    .attr('stroke-width', 2)
+
+  // Axis
+  const yAxis = d3.axisLeft(yScale)
+    .tickFormat((d) => `$${d}`)
+
+  ctr.append('g')
+    .call(yAxis)
+
+  const xAxis = d3.axisBottom(xScale)
+  
+  ctr.append('g')
+      .style('transform', `translateY(${dimensions.ctrHeight}px)`)
+    .call(xAxis)
+
 }
 
 draw()
