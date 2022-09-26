22488e50f2704a75fcb29a0a7c9511cd46427940
maxmay94 <maxmay94@gmail.com>
2022-09-26 11:46:41 -0400

    change sorting, move labels

--- 7-10/app.js
+++ 7-10/app.js
@@ -28,12 +28,19 @@ async function draw() {
   // Scales
   const populationPie = d3.pie()
     .value((d) => d.value)
+    .sort(null)
   
   const slices = populationPie(dataset)
 
   const arc = d3.arc()
     .outerRadius(radius)
     .innerRadius(0)
+
+  const arcLabels = d3.arc()
+    .outerRadius(radius)
+    .innerRadius(200)
+  
+
 
   const colors = d3.quantize(d3.interpolateSpectral, dataset.length)
   const colorScale = d3.scaleOrdinal()
@@ -64,15 +71,20 @@ async function draw() {
   labelsGroup.selectAll('text')
     .data(slices)
     .join('text')
-    .attr('transform', (d) => `translate(${arc.centroid(d)})`)
+    .attr('transform', (d) => `translate(${arcLabels.centroid(d)})`)
     .call(
       text => text.append('tspan')
         .style('font-weight', 'bold')
+        .attr('y', -4)
         .text(d => d.data.name)
     )
     .call(
-      text => text.append('tspan')
-      .text(d => d.data.value)
+      text => text.filter(d => (d.endAngle - d.startAngle) > 0.25)
+        .append('tspan')
+        .attr('y', 9)
+        .attr('x', 0)
+        .text(d => d.data.value)
+
     )
 }
 
